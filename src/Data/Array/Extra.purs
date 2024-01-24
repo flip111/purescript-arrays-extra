02e0c507c1096b3daeae6d1415080ad539048ca8
flip111 <flip101@gmail.com>
2024-01-24 22:05:42 +0100

    Remove functions from foldable-traversable-extra
    * Add sortByMultiple
    * Add interleave
    * Bugfix combinations

--- src/Data/Array/Extra.purs
+++ src/Data/Array/Extra.purs
@@ -1,32 +1,62 @@
 -- | Some specialized functions can be found here.
 
-module Data.Array.Extra where
+module Data.Array.Extra
+  ( sortByMultiple
+  , sortOn
+  , sortOn'
+  , sortOnBy
+  , sortOnBy'
+  , sortOnByMaybe
+  , sortOnByMaybe'
+  , differenceBy
+  , partitionEithers
+  , unionByWhen
+  , combinations
+  , zipIndex
+  , interleave
+  , module Data.Foldable.Extra
+  , module Data.Semigroup.Foldable.Extra
+  , module Data.Traversable.Extra
+  ) where
 
-import Control.Applicative (pure)
 import Control.Semigroupoid ((<<<))
-import Data.Array (all, deleteBy, elem, filter, foldl, foldr, intersectBy, length, null, snoc, sortBy, unionBy, uncons, zipWith, cons, range)
-import Data.Array.Extra.First (modifyOrSnoc)
+import Data.Array (catMaybes, cons, deleteBy, filter, foldr, intersectBy, length, range, sortBy, uncons, unionBy, zipWith)
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Array.NonEmpty as NEA
-import Data.Either (Either(..))
-import Data.Eq (class Eq, (==), (/=))
+import Data.Either (Either)
 import Data.Filterable (partitionMap)
+import Data.Foldable (fold)
+import Data.Foldable.Extra
 import Data.Function (on)
 import Data.Functor (map)
 import Data.HeytingAlgebra ((||))
-import Data.List as List
-import Data.Map (Map)
-import Data.Map as Map
 import Data.Maybe (Maybe(..), fromJust)
-import Data.Monoid (mempty)
 import Data.Ord (class Ord, comparing, (<), (>))
 import Data.Ordering (Ordering(..))
 import Data.Ring ((-))
 import Data.Semigroup ((<>))
-import Data.Semiring ((+))
-import Data.Traversable (traverse)
+import Data.Semigroup.Foldable.Extra
+import Data.Traversable.Extra
 import Data.Tuple (Tuple(..), fst, snd)
 import Partial.Unsafe (unsafePartial)
+
+
+-- | Sort by multiple comparison functions.
+-- | Similar to SQL where you can specificy multiple columns with ASC and DESC.
+-- | When a comparison function gives EQ the next one is tried.
+-- |
+-- | Other functions don't have a Multiple variant such as sortOnByMultiple. Because the implementation of this function is so simple it serves as example function.
+-- | `sortByMultiple comps xs = sortBy (fold comps) xs`
+-- |
+-- | ```purescript
+-- | let datas = [{a: 2, b: 4}, {a: 3, b: 4}, {a: 2, b: 5}, {a: 1, b: 5}]
+-- |     a {a: a1} {a: a2} = compare a1 a2
+-- |     b {b: b1} {b: b2} = compare b1 b2 -- use flip for DESC, or alternatively: compare b2 b1
+-- | in  sortByMultiple [a, flip b] datas = [{a: 1, b: 5}, {a: 2, b: 5}, {a: 2, b: 4}, {a: 3, b: 4}]
+-- | ```
+sortByMultiple :: forall a. Array (a -> a -> Ordering) -> Array a -> Array a
+sortByMultiple comps xs = sortBy (fold comps) xs
+
 
 -- | Sort a list by a projection.
 -- |
@@ -68,6 +98,7 @@ sortOnBy' f comp = map snd <<< sortBy (comp `on` fst) <<< map (\x -> let y = f x
 -- | ```purescript
 -- | sortOnByMaybe (\x -> if x == "dog" then Just 2 else Just 1) compare ["apple", "dog", "kiwi"] = ["apple", "kiwi", "dog"]
 -- | ```
+-- Implementers note: this could be simplified with https://github.com/purescript/purescript-prelude/issues/310
 sortOnByMaybe :: forall a b. (a -> Maybe b) -> (b -> b -> Ordering) -> Array a -> Array a
 sortOnByMaybe f comp xs =
   let g Nothing  Nothing  = EQ
@@ -98,18 +129,6 @@ sortOnByMaybe' f comp xs =
 -- | ```
 differenceBy :: forall a. (a -> a -> Boolean) -> Array a -> Array a -> Array a
 differenceBy eq xs ys = foldr (deleteBy eq) xs ys
-
--- | Try to make a projection from an array. Return an array with the projection and the original array with the elements removed.
--- |
--- | ```purescript
--- | partitionMaybe (\x -> if x == "dog" then Just "cat" else Nothing) ["apple", "dog", "kiwi"] == { no: ["apple", "kiwi"], yes: ["cat"] }
--- | ```
-partitionMaybe :: forall a b. (a -> Maybe b) -> Array a -> { no :: Array a, yes :: Array b }
-partitionMaybe f xs = foldl go {yes: [], no: []} xs
-  where go rec@{yes, no} x =
-          case f x of
-            Nothing -> rec {no  = no <> [x]}
-            Just b  -> rec {yes = yes <> [b]}
 
 -- | Partitions an array of Either into two arrays. All the Left elements are put, in order, into the left field of the output record. Similarly the Right elements are put into the right field of the output record.
 -- | Note that this function is an alias for `partitionMap` from `Data.Filterable`.
@@ -144,131 +163,6 @@ unionByWhen eq f array_a array_b =
         n = filter f (differenceBy eq array_a array_b)
     in unionBy eq n i
 
--- | Map an array conditionally, only return the array when at least one element was mapped.
--- | Elements that are not mapped will keep the old value.
--- |
--- | ```purescript
--- | mapMaybeAny (\_ -> Nothing) [1,2,3] == Nothing
--- | mapMaybeAny (\x -> if x == 2 then Just 99 else Nothing) [1,2,3] == Just [1,99,3]
--- | ```
-mapMaybeAny :: forall a. (a -> Maybe a) -> Array a -> Maybe (Array a)
-mapMaybeAny f xs =
-  let go (Tuple acc replaced) x = case f x of
-          Nothing -> Tuple (acc <> pure x) replaced
-          Just y  -> Tuple (acc <> pure y) true
-
-      Tuple acc replaced = foldl go (Tuple mempty false) xs
-
-  in  if replaced then
-        Just acc
-      else
-        Nothing
-
--- | Map an array conditionally, only return the array when all elements were mapped.
--- | Note that this function is an alias for `traverse` from `Data.Traversable`. This is specific behavior for the implementation of `<*>` for `Applicative Maybe`.
--- |
--- | ```purescript
--- | mapMaybeAll (\x -> if x == 2 then Just 99 else Nothing) [1,2,3] == Nothing
--- | mapMaybeAll (\x -> Just (x * 2)) [1,2,3] == Just [2,4,6]
--- | ```
-mapMaybeAll :: forall a b. (a -> Maybe b) -> Array a -> Maybe (Array b)
-mapMaybeAll = traverse
-
--- | Map with a function that yields `Either`. Only succeeding when all elements where mapped to `Right`.
--- | Hint: if you don't care about collecting all the Left's (error conditions) and you are looking for a function like
--- | `forall a b c. (a -> Either c b) -> Array a -> Either c (Array b)` then use `traverse` from `Data.Traversable`.
-mapEither :: forall a b c. (a -> Either c b) -> Array a -> Either (Array c) (Array b)
-mapEither f foldable =
-  let {lefts, rights} = foldl (g f) {lefts: [], rights: []} foldable
-  in  if null lefts then
-          Right rights
-        else
-          Left lefts
-  where g :: forall q r s. (q -> Either s r) -> {lefts :: Array s, rights :: Array r} -> q -> {lefts :: Array s, rights :: Array r}
-        g f2 {lefts, rights} elem = case f2 elem of
-          Left l -> {lefts: snoc lefts l, rights}
-          Right r -> {lefts, rights: snoc rights r}
-
--- | Count the amount of times a value occurs in an array.
--- | Mostly useful for when you can not define an Ord instance
--- |
--- | ```purescript
--- | occurrences ["A", "B", "B"] == [Tuple "A" 1, Tuple "B" 2]
--- | ```
-occurrences :: forall a. Eq a => Array a -> Array (Tuple a Int)
-occurrences xs = foldl go [] xs
-  where go acc x = modifyOrSnoc (\(Tuple k _) -> k == x) (\(Tuple k v) -> Tuple k (v + 1)) acc (Tuple x 1)
-
--- | Count the amount of times a value occurs in an array.
--- | Requires an Ord instance for Map. This function should be faster than `occurrences`
--- |
--- | ```purescript
--- | occurrencesMap ["A", "B", "B"] == Map.fromList [Tuple "A" 1, Tuple "B" 2]
--- | ```
-occurrencesMap :: forall a. Ord a => Array a -> Map a Int
-occurrencesMap xs = foldl go Map.empty xs
-  where go acc x = Map.insertWith (\old _ -> old + 1) x 1 acc
-
--- | Checks if two arrays have exactly the same elements.
--- | The order of elements does not matter.
--- |
--- | ```purescript
--- | sameElements ["A", "B", "B"] ["B", "A", "B"] == true
--- | sameElements ["A", "B", "B"] ["A", "B"] == false
--- | ```
-sameElements :: forall a. Eq a => Array a -> Array a -> Boolean
-sameElements a b = if length a /= length b then false else
-  let occ_a = occurrences a
-      occ_b = occurrences b
-      go :: Tuple a Int -> Boolean
-      go x = x `elem` occ_b
-  in  all go occ_a
-
--- | Similar to `group`, adds the ability to group by a projection.
--- | The projection is returned as the first argument of the Tuple.
--- |
--- | ```purescript
--- | groupMaybe (\x -> Just $ if even x then "even" else "odd") [1,2,3] == [(Tuple "odd" [1,3]), (Tuple "even" [2])]
--- | ```
-groupMaybe :: forall a b. Eq b => (a -> Maybe b) -> Array a -> Array (Tuple b (NonEmptyArray a))
-groupMaybe f xs =
-  let g :: Array (Tuple b (NonEmptyArray a)) -> a -> Array (Tuple b (NonEmptyArray a))
-      g acc x = case f x of
-        Nothing -> acc
-        -- modifyOrSnoc :: forall a. (a -> Boolean) -> (a -> a) -> Array a -> a -> Array a
-        Just v  -> modifyOrSnoc (\(Tuple acc_b _) -> acc_b == v) (\(Tuple acc_b nea) -> Tuple acc_b (NEA.snoc nea x)) acc (Tuple v (NEA.singleton x))
-  in  foldl g [] xs
-
--- | Similar to `groupMaybe`, adds the ability to map over the thing being grouped.
--- | Useful for removing data that was only there to do the grouping.
--- |
--- | ```purescript
--- | groupMaybeMap (\x -> Just $ if even x then "even" else "odd") (*3) [1,2,3] == [(Tuple "odd" [3,9]), (Tuple "even" [6])]
--- | groupMaybeMap f identity xs = groupMaybe f xs
--- | ```
-groupMaybeMap :: forall a b c. Eq b => (a -> Maybe b) -> (a -> c) -> Array a -> Array (Tuple b (NonEmptyArray c))
-groupMaybeMap f g xs =
-  let h :: Array (Tuple b (NonEmptyArray c)) -> a -> Array (Tuple b (NonEmptyArray c))
-      h acc x = case f x of
-        Nothing -> acc
-        Just v  -> modifyOrSnoc (\(Tuple acc_b _) -> acc_b == v) (\(Tuple acc_b nea) -> Tuple acc_b (NEA.snoc nea (g x))) acc (Tuple v (NEA.singleton (g x)))
-  in  foldl h [] xs
-
--- | Similar to `groupAllBy` but combines all duplicates into one element given a function
--- |
--- | ```purescript
--- | let products = [{product: "bread", amount: 1}, {product: "cheese", amount: 2}, {product: "bread", amount: 3}]
--- | in combineOrd (_ .product) (\a b -> {product: a.product, amount: a.amount + b.amount}) products == [{product: "bread", amount: 4}, {product: "cheese", amount: 2}]
--- | ```
-combineOrd :: forall a b. Ord b => (a -> b) -> (a -> a -> a) -> Array a -> Array a
-combineOrd f g xs = List.toUnfoldable (Map.values (foldl h Map.empty xs))
-  where h :: Map b a -> a -> Map b a
-        h acc x = Map.alter k (f x) acc
-          where k :: Maybe a -> Maybe a
-                k Nothing  = Just x
-                k (Just y) = Just (g x y)
--- maybe in the future f :: forall a b. Ord b => (a -> b) -> (a -> c -> c) -> Array a -> Array c can be added
-
 -- | Get all combinations when drawing n elements from an array.
 -- | n must be greater than 0 and not greater than the length of the array otherwise this function returns `Nothing`
 -- |
@@ -278,7 +172,8 @@ combineOrd f g xs = List.toUnfoldable (Map.values (foldl h Map.empty xs))
 -- | ```
 combinations :: forall a. Int -> Array a -> Maybe (Array (NonEmptyArray a))
 combinations n xs =
-  let f nn xs' = case uncons xs' of
+  let f 0  _   = [[]]
+      f nn xs' = case uncons xs' of
         Nothing           -> []
         Just {head, tail} -> map (cons head) (f (nn - 1) tail) <> f n tail
   in  if n < 1 || n > length xs then
@@ -293,3 +188,18 @@ combinations n xs =
 -- | ```
 zipIndex :: forall a. Array a -> Array (Tuple Int a)
 zipIndex xs = zipWith Tuple (range 0 (length xs)) xs
+
+-- | Takes an element from each array in turn to produce a new array.
+-- |
+-- | ```purescript
+-- | interleave [[1,2], [3,4]] = [1,3,2,4]
+-- | concat     [[1,2], [3,4]] = [1,2,3,4]
+-- | ```
+interleave :: forall a. Array (Array a) -> Array a
+interleave xss =
+  let f :: Tuple (Array (Array a)) (Array a) -> Tuple (Array (Array a)) (Array a)
+      f (Tuple [] acc)  = Tuple [] acc
+      f (Tuple xss' acc) =
+        let heads_tails = catMaybes (map uncons xss')
+        in  f (Tuple (map (_.tail) heads_tails) (map (_.head) heads_tails <> acc))
+  in  snd (f (Tuple xss []))
